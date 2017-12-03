43a8686088822dc9e25cdcd88d546c9dcea68a20
kornilova <kornilova203@gmail.com>
2017-12-03 16:15:04 +0200

    ProgressBar predicts time

--- src/main/kotlin/com/github/kornilova_l/util/ProgressBar.kt
+++ src/main/kotlin/com/github/kornilova_l/util/ProgressBar.kt
@@ -15,6 +15,7 @@ class ProgressBar(private val total: Int) {
         timer.schedule(object : TimerTask() {
             override fun run() {
                 updateProgress(0)
+                redraw()
             }
         }, 0, 1000)
     }
@@ -23,7 +24,41 @@ class ProgressBar(private val total: Int) {
     fun finish() {
         timer.cancel()
         updateProgress(total - current)
+        redraw()
         println()
+    }
+
+    fun redraw() {
+        var percent = current * 100 / total.toDouble()
+        percent /= 2
+        val resizingTotal = 50
+        val string = StringBuilder(140)
+        percent = if (percent == 0.toDouble()) 0.01 else percent
+        val intPercent = Math.ceil(percent).toInt()
+        val format = DecimalFormat("####")
+        val timePassed = lastUpdateTime - startTime
+        string.append('\r')
+                .append(Collections.nCopies(2 - Math.log10((intPercent * 2).toDouble()).toInt(), " ").joinToString(""))
+                .append(String.format("%.2f%% [", percent * 2))
+                .append(Collections.nCopies(intPercent, "=").joinToString(""))
+                .append('>')
+                .append(Collections.nCopies(resizingTotal - intPercent, " ").joinToString(""))
+                .append(']')
+                .append(String.format("%4s", format.format(timePassed / 1000)))
+                .append("s")
+
+        if (timePassed / 1000 > 0 && current > 0) { // predict time
+            val timePerElement = timePassed.toDouble() / 1000 / current
+            val totalTime = timePerElement * total
+            string
+                    .append(" / ")
+                    .append(String.format("%4s", format.format(totalTime / 60)))
+                    .append("m ")
+                    .append(format.format(totalTime % 60))
+                    .append("s")
+        }
+
+        print(string)
     }
 
     @Synchronized
@@ -33,23 +68,5 @@ class ProgressBar(private val total: Int) {
         }
         current += addToProgress
         lastUpdateTime = System.currentTimeMillis()
-        var percent = current * 100 / total.toDouble()
-        percent /= 2
-        val resizingTotal = 50
-        val string = StringBuilder(140)
-        percent = if (percent == 0.toDouble()) 0.01 else percent
-        val intPercent = Math.ceil(percent).toInt()
-        val format = DecimalFormat("####")
-        string.append('\r')
-                .append(Collections.nCopies(2 - Math.log10((intPercent * 2).toDouble()).toInt(), " ").joinToString(""))
-                .append(String.format("%.2f%% [", percent * 2))
-                .append(Collections.nCopies(intPercent, "=").joinToString(""))
-                .append('>')
-                .append(Collections.nCopies(resizingTotal - intPercent, " ").joinToString(""))
-                .append(']')
-                .append(String.format("%4s", format.format((lastUpdateTime - startTime).toDouble() / 1000)))
-                .append("s")
-
-        print(string)
     }
 }
