4e46cd9dab6e76374708a33df68828f7376a1cc2
kornilova <kornilova203@gmail.com>
2017-12-11 16:13:59 +0200

    Implemented method that generates all combination in memory

--- src/main/kotlin/com/github/kornilova_l/TwoOrThreeNeighbours.kt
+++ src/main/kotlin/com/github/kornilova_l/TwoOrThreeNeighbours.kt
@@ -8,10 +8,24 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.FileWriter
 import java.util.*
+import kotlin.collections.HashSet
 
 var count = 0
 
 fun main(args: Array<String>) {
+    val rules = ArrayList<VertexRule>()
+    rules.addAll(getRulePermutations(2, true))
+    rules.addAll(getRulePermutations(3, true))
+    rules.addAll(getRulePermutations(2, false))
+    rules.addAll(getRulePermutations(3, false))
+    println(rules.size)
+
+    val res = HashSet<Set<VertexRule>>()
+    getAllRuleCombination(rules, ArrayList(rules.size), res)
+    println(res)
+}
+
+fun scanFromFile() {
     Scanner(FileInputStream(File("lists_of_rules/two_or_three_neighbours.txt"))).use { scanner ->
         while (scanner.hasNextLine()) {
             val line = scanner.nextLine()
@@ -19,7 +33,7 @@ fun main(args: Array<String>) {
                 continue
             }
             val startTime = System.currentTimeMillis()
-            val rules = line.split(" ").filter{ ruleString -> ruleString != "" }
+            val rules = line.split(" ").filter { ruleString -> ruleString != "" }
                     .map { ruleString -> VertexRule(ruleString) }.toSet()
             println(rules)
             val labelingFunction = getLabelingFunction(rules)
@@ -69,3 +83,27 @@ fun outputAllRuleCombination(rules: List<VertexRule>, isIncludedArr: ArrayList<B
     isIncludedArr.add(false)
     outputAllRuleCombination(rules, isIncludedArr, writer)
 }
+
+fun getAllRuleCombination(rules: List<VertexRule>, isIncludedArr: ArrayList<Boolean>, res: MutableSet<Set<VertexRule>>) {
+    if (isIncludedArr.size == rules.size) {
+        count++
+        if (count % 100000 == 0) {
+            println(count)
+        }
+        val set = HashSet<VertexRule>()
+        isIncludedArr.forEachIndexed { index, isIncluded ->
+            if (isIncluded) {
+                set.add(rules[index])
+            }
+        }
+        res.add(set)
+        return
+    }
+    val isIncludedArr2 = ArrayList(isIncludedArr)
+    isIncludedArr2.add(true)
+    getAllRuleCombination(rules, isIncludedArr2, res)
+
+    isIncludedArr.add(false)
+    getAllRuleCombination(rules, isIncludedArr, res)
+}
+
